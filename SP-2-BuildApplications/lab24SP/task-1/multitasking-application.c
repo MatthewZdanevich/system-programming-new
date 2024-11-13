56ec5751e849799b3ba1e542f416e216b0bdf014
Matthew Zdanevich <mzdanevich@outlook.com>
2024-11-13 23:56:14 +0300

    2-4

---
+++ SP-2-BuildApplications/lab24SP/task-1/multitasking-application.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define DELAY 5
+
+void process_information(void) {
+	pid_t pid = fork();
+
+	if (pid == 0) {
+		printf("Child process\tPID: %d\tFork PID: %d\n", getpid(), pid);
+		exit(0);
+	}
+	else if (pid > 0) {
+		wait(NULL);
+		printf("Parent process\tPID: %d\tFork PID: %d\n", getpid(), pid);
+	}
+	else {
+		fprintf(stderr, "Error: fork()\n");
+	}
+}
+
+void process_waiting(void) {
+	pid_t pid = fork();
+
+	if (pid == 0) {
+	        printf("Start\tChild process\tPID: %d\n", getpid());
+		printf("Delay 5 seconds ");
+		for (int i = 0; i < DELAY; i++) {
+			printf(".");
+			fflush(stdout);
+			sleep(1);
+		}
+		printf("\nEnd\tChild process\tPID: %d\n", getpid());
+                exit(0);
+	}
+	else if (pid > 0) {
+		int status;
+                waitpid(pid, &status, 0);
+
+		printf("Start\tParent process\tPID: %d\n", getpid());
+		if (WIFEXITED(status)) {
+			printf("Child process exited with code: %d\n", WEXITSTATUS(status));
+		}
+		else {
+			printf("Child process did not exit normally\n");
+		}
+		printf("End\tParent process\tPID: %d\n", getpid());
+	}
+	else {
+		fprintf(stderr, "Error: fork()\n");
+	}
+}
+
+void process_niceness(void) {
+	int current_niceness = nice(0);
+	if (current_niceness == -1) {
+		fprintf(stderr, "Error: nice()\n");
+	}
+	else {
+		printf("Process niceness: %d\n", current_niceness);
+	}
+
+	int new_niceness = nice(1);
+	if (new_niceness == -1) {
+		fprintf(stderr, "Error: nice()\n");
+	}
+	else {
+		printf("New process niceness: %d\n", new_niceness);
+	}
+}
+
+void process_zombie(void) {
+        pid_t pid = fork();
+
+        if (pid == 0) {
+                printf("Start\tChild process\tPID: %d\n", getpid());
+                fflush(stdout);
+		sleep(2);
+		printf("End\tChild process\tPID: %d\n", getpid());
+		fflush(stdout);
+		exit(0);
+        }
+        else if (pid > 0) {
+		printf("Start\tParent process\tPID: %d\n", getpid());
+           	fflush(stdout);
+		sleep(10);
+                printf("End\tParent process\tPID: %d\n", getpid());
+		fflush(stdout);
+		wait(NULL);
+        }
+        else {
+                fprintf(stderr, "Error: fork()\n");
+        }
+}
+
+int main(void) {
+	printf("Launching a multitasking application ...\n");
+
+	// Получение данных о процессах
+	printf("\n[RETRIEVING PROCESS DATA]\n");
+	process_information();
+
+	// Ожидание завершение процесса
+	printf("\n[WAITING FOR THE PROCESS TO COMPLETE]\n");
+	process_waiting();
+
+	// Уступчивость процесса
+	printf("\n[PROCESS NICENESS]\n");
+	process_niceness();
+
+	// Процессы-зомби
+	printf("\n[ZOMBIE PROCESSES]\n");
+	process_zombie();
+
+	printf("\nTurn off a multitasking application ...\n");
+	return 0;
+}
---
+++ SP-2-BuildApplications/lab24SP/task-1/view/process_representation.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "process_representation.h"
+
+void print_title(const char *message) {
+	printf("\n%s\n", message);
+}
